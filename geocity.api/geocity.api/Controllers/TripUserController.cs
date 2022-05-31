b87b1b68b1f38df7b3db648e4b8a5896c55c79b5
WilliamGordon <william.wauters@outlook.com>
2022-05-31 10:23:10 +0200

    test

--- geocity.api/geocity.api/Controllers/TripUserController.cs
+++ geocity.api/geocity.api/Controllers/TripUserController.cs
@@ -43,6 +43,7 @@ namespace geocity.api.Controllers
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex.Message);
                 return BadRequest(ex.Message);
             }
         }
